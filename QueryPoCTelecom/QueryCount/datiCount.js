8180ae7070ef5fee5e8fa820673ba9da1391fea5
Berand <42213146+andreaberri@users.noreply.github.com>
2018-11-09 19:57:38 +0100

    Add files via upload
    Query per il count dei record

---
+++ QueryPoCTelecom/QueryCount/datiCount.js
@@ -0,0 +1,6 @@
+db.[nomeDB].aggregate([
+{$match :{"numLinea":"3354323423", "data":{$gte: "20180101", $lte: "20181231"}}},
+{ $unwind: "$cdrList" },
+{$match: {"cdrList.macroFamiglia" : "DATI", "cdrList.dataChiamata": { $gte: "20180101000000", $lt: "20181231999999"}}},
+{$count: "risultatiTrovati"}
+]).pretty()
