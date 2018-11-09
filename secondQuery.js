c677f0222b2ef02ae83a67db179053f9e73e8191
Berand <42213146+andreaberri@users.noreply.github.com>
2018-11-09 12:31:50 +0100

    Update secondQuery.js
    file di variabili js

--- secondQuery.js
+++ secondQuery.js
@@ -1,11 +1,11 @@
 //SECOND QUERY#################################
 
-var numLinea= "354323423"; // stringa identificativa del chiamante es. "354323423"
+var numLinea= "3354323423"; // stringa identificativa del chiamante es. "354323423"
 var dateStart= "20180101"; // data in formato stringa yyyyMMdd
 var dateStop= "20181231"; // data in formato stringa yyyyMMdd
-var cdrsType= "SMS"; //tipo di cdrs da cercare es. "APN"
-var tsStart= "20180101000000",; //formato stringa timestamp da cui cercare il cdrs es. "20181101150000"
-var tsStop= "20181231999999",; //formato stringa timestamp da cui cercare il cdrs es. "20181101150000"
+var cdrsType= "DATI"; //tipo di cdrs da cercare es. "VOCE", SMS/MMS, DATI
+var tsStart= "20180101000000"; //formato stringa timestamp da cui cercare il cdrs es. "20181101150000"
+var tsStop= "20181231999999"; //formato stringa timestamp da cui cercare il cdrs es. "20181101150000"
 
 
 match0= {
@@ -13,10 +13,6 @@ match0= {
 		"numLinea" : numLinea, 
 		"data":{$gte: dateStart, $lte: dateStop}
 		}
-	}
-	
-unwind= {
-	$unwind: "$cdrList"
 	}
 	
 match1= {
@@ -30,6 +26,18 @@ sort= {
 	$sort: { "cdrList.dataChiamata": 1 } 
 	}
 
+count=  {
+	$count: "progIdTypeInCdrs"
+	}
+	
+group= {
+	$group : {                                       
+     _id : { numLinea: "$numLinea", data : "$data"},     
+     totalField1: { $sum: "$cdrList.f1" },
+     totalField3: { $sum: "$cdrList.f3" }
+         }
+	}
 
 	
-db.bucketProva.aggregate([match0, unwind, match1, sort]).pretty()
+
+//db.bucketProva.aggregate([match0, match1, sort]).pretty() esempio di uso
